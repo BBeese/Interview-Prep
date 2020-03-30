eb5093138cd66bab71c568ebaf06496f7fb6fd5b
Brett B <36874395+BBeese@users.noreply.github.com>
2020-03-29 20:32:09 -0500

    merge 2 binary trees

--- Scripts/trees.py
+++ Scripts/trees.py
@@ -63,6 +63,24 @@ def closest_node_to_target(root, target):
     x = closest_node_to_target(child, target)
     return min(abs(root.val - target), abs(x - target))
 
+# Merge 2 binary trees. sum value at each node
+def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+    if not t1 and not t2:
+        return None
+    
+    if not t1:
+        return t2
+        
+    if not t2:
+        return t1
+        
+    root = TreeNode(t1.val + t2.val)
+    root.left = self.mergeTrees(t1.left, t2.left)
+    root.right = self.mergeTrees(t1.right, t2.right)
+    
+    return root
+        
+
 def main():
     t = Tree()
     t.insert(t.root, 1)
